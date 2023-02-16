0c39cc859d20d73266df94fa883e53e7f267161c
Jasper Schelfhout <jschelfhout@openanalytics.eu>
2023-02-16 08:39:37 +0100

    resolve test logs warnings

--- editbl/tests/testthat/test-demoApp.R
+++ editbl/tests/testthat/test-demoApp.R
@@ -1,11 +1,14 @@
 test_that("Starting demo app db works", {
       runDemoApp (app = "database")
+      expect_true(TRUE)
     })
 
 test_that("Starting demo app mtcars works", {
       runDemoApp (app = "mtcars")
+      expect_true(TRUE)
     })
 
 test_that("Starting demo app custom works", {
       runDemoApp(app = "custom", dplyr::tibble(iris))
+      expect_true(TRUE)
     })
